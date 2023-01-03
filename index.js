a312270449f913d0642e96939caf07c6f6c343b3
deloreanyousef <121785192+deloreanyousef@users.noreply.github.com>
2023-01-02 20:55:17 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+function wassup(name, height, country) {
+  alert('Hey! My name is ' + name + ". I am " + height + " tall. I live in Inglewood, CA which is in " + country);
+}
+
+wassup ('DeLorean', '5 feet 8 inches', 'the United States of America.')
